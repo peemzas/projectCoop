de90f7fb4697f0c4170b182cc6463e81edbd7e48
PTangtanakul <P.tangtanakul@hotmail.com>
2015-08-25 09:22:20 +0700

    category and Subcategory Template Complete(select)

--- main/webapp/resources/js/pageScript/exam/selectCategoryInput.js
+++ main/webapp/resources/js/pageScript/exam/selectCategoryInput.js
@@ -1,8 +1,55 @@
-var getSearchCategoryInput = function(){
+var getSearchCategoryInput = function () {
     return $('#selectCategoryInput-category').val();
 }
 
-var getSearchSubCategoryInput = function(){
+var getSearchSubCategoryInput = function () {
     return $('#selectCategoryInput-subCategory').val();
 }
 
+var updateCategoryList = function () {
+    $('#selectCategoryInput-category').children('.category').remove()
+    var ajaxDat = $.ajax({
+        type: "POST",
+        url: "/TDCS/exam/getAllCategory",
+        success: function (catList) {
+            catList.forEach(function (category) {
+                $('#selectCategoryInput-category').append('<option class="category" categoryId="'+category.id+'"' +
+                ' categoryName="'+category.name+'"> '+category.id+' : '+category.name+'')
+
+            })
+        },
+        error: function(){
+            alert("error in updateCategoryList()")
+        }
+    })
+}
+
+var updateSubCategoryList = function(){
+    var ajaxDat = $.ajax({
+        type: "POST",
+        url: "/TDCS/exam/getAllSubCategoryInCategory",
+        date:{
+            categoryId: $('#selectCategoryInput-category').children('.category:selected').attr('categoryId')
+        },
+        success: function (item) {
+            alert("success")
+            item.forEach(function (item) {
+                $('#selectCategoryInput-subCategory').append('<option class="subCategory" subCategoryId="'+item.subId+'"' +
+                ' subCategoryName="'+item.subName+'" categoryId="'+item.id+'"' +
+                'value = "'+item.subId+'"> '+item.subName+'')
+            })
+        },
+        error: function(){
+            alert("error in updateSubCategoryList()")
+        }
+    })
+}
+
+$(document).ready(function(){
+    updateCategoryList()
+
+    $('#selectCategoryInput-category').on('change',function(){
+        $('#selectCategoryInput-subCategory').children('.subCategory').remove()
+        updateSubCategoryList()
+    })
+})
